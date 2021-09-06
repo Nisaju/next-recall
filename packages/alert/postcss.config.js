2e05fa176de7b1944a86fb38ed590976c96cb157
Anjianto <anjianto06@gmail.com>
2021-09-06 16:51:25 +0700

    add: plugins
    * postcss-nested
    * cssnano

--- packages/alert/postcss.config.js
+++ packages/alert/postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('postcss-nested'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: { removeAll: true },
+          discardDuplicates: {},
+          cssDeclarationSorter: {},
+          colormin: {},
+        },
+      ],
+    }),
+  ],
+};
